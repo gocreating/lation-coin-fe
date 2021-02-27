ea41a37b19ea66755b16434a64de03763c4253e7
gocreating <gocreating@gmail.com>
2021-02-27 15:11:05 +0800

    Setup configs

---
+++ utils/config.js
@@ -0,0 +1,11 @@
+export const GA_TRACKING_ID = 'G-2QF0SZ7GJV'
+export const ADSENSE_CLIENT_ID = 'ca-pub-3783614920655833'
+export const adUnitIdMap = {
+  adunit_stock_banner: '9771574275',
+}
+export const FACEBOOK_PAGE_ID = '100157145366403'
+export const API_HOST = (
+  process.env.NODE_ENV === 'production'
+    ? 'https://coin-api.lation.app:8888'
+    : 'http://localhost:8000'
+)
