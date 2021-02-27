2c38b8dc862401e8698ed6381217ca3eede5997e
gocreating <gocreating@gmail.com>
2021-02-27 15:23:39 +0800

    Fix adunit name

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 export const GA_TRACKING_ID = 'G-2QF0SZ7GJV'
 export const ADSENSE_CLIENT_ID = 'ca-pub-3783614920655833'
 export const adUnitIdMap = {
-  adunit_stock_banner: '9771574275',
+  adunit_coin_banner: '9771574275',
 }
 export const FACEBOOK_PAGE_ID = '100157145366403'
 export const API_HOST = (
